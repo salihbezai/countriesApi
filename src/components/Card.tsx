090a63572c8ef1762938e979d9e866ed969cc0dc
salihbezai <salihbezai98@gmail.com>
2024-11-23 23:01:09 +0100

    fixed some wrapping divs && remove unuseful rounded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Card = ({country}:any) => {
   return (
-    <div className='w-full h-full bg-white dark:bg-darkblue  transition-all rounded-md overflow-hidden 
+    <div className='w-full h-full  dark:bg-darkBlueElements  transition-all rounded-md overflow-hidden 
     cursor-pointer shadow-md hover:shadow-lg translate-y-6 hover:scale-105 max-h-[25rem] max-w-sm mx-auto'>
-        <img className='w-full h-1/2  object-cover rounded-md' src={country.flags.svg} alt={country.flags.alt} />
+        <img className='w-full h-1/2  object-cover' src={country.flags.svg} alt={country.flags.alt} />
         <div className='p-6 h-1/2 dark:bg-darkBlueElements dark:text-whiteText'>
             <h2  className='font-extrabold text-xl overflow-hidden'>{country.name.common}</h2>
             <div className='flex items-center '>
--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -44,29 +44,28 @@ const page =   ({ name }: {name:string}) => {
     
     console.log("the country is "+JSON.stringify(country))
 
-    if(isLoading) return(
+    if(isLoading)return(
         <div role="status" className='flex justify-center items-center h-screen'>
-            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-veryDarkBlueText" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg aria-hidden="true" className="w-8 h-8 dark:text-whiteText text-gray-200 animate-spin dark:fill-darkBlueElements  fill-veryDarkBlueText" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
             </svg>
-            <span className="sr-only">Loading...</span>
         </div>
-    )    
+    )     
     if(!country && !isLoading) return notFound()
   return (
     <div className='w-full'>
-        <Button variant="ghost" className='mt-2 px-4 py-2' >
+        <Button variant="ghost" className='mt-2 px-4 py-2 shadow-lg active:scale-95 hover:opacity-80' >
             <Link className='flex items-center text-veryDarkBlueText dark:text-whiteText' href='/'>
                 <ArrowLeft className='mr-2'/>
                 Back
              </Link>
         </Button>
-        <div className='w-full py-5  mobile:flex-col flex mobile:items-start md:items-center lg:flex-row  md:gap-10 lg:gap-20'>
-            <div className='w-full flex-1'>
-                <img className='w-full' src={country && country.flags.svg} alt={country ? country.name.common:'flag'} />
-            </div>
-            <div className='py-10 space-y-5 flex-1 flex-col items-center justify-center '>
+        <div className='w-full py-5   mobile:flex-col flex mobile:items-start md:items-center lg:flex-row  md:gap-6 lg:gap-20'>
+            
+                <img className='sm:w-1/2 object-contain max-h-[22rem]' src={country && country.flags.svg} alt={country ? country.name.common:'flag'} />
+       
+            <div className='py-10 space-y-5  flex-col items-center justify-center '>
             <h1  className='font-extrabold md:text-2xl text-veryDarkBlueText dark:text-whiteText'>{country && country.name.common}</h1>
 
                 <div className='w-full flex  mobile:flex-col
@@ -116,15 +115,15 @@ const page =   ({ name }: {name:string}) => {
                 </div>
 
                 <div className='flex mobile:flex-col sm:flex-col md:flex-row'>
-                    <div className='flex flex-row items-center gap-2'>
+                    <div className='flex flex-row items-center gap-2 mobile:flex-wrap sm:flex-wrap '>
                     <span className='text-body  font-extralight mr-1 text-veryDarkBlueText dark:text-whiteText'>Border Countries:</span>
                     {
                       country && country.borders ? (country.borders.map((border)=>(
-                        <span className='text-body flex items-center justify-center  
-                        font-light bg-white rounded border-zinc-400 shadow-lg px-10 py-1
-                         text-veryDarkBlueText dark:text-whiteText'>{border}</span>
+                        <span className='text-sm flex items-center justify-center sm:flex-wrap  
+                        font-light cursor-pointer bg-white rounded border-zinc-400 shadow-lg px-10 py-1
+                         text-veryDarkBlueText dark:text-veryDarkBlueText hover:opacity-80 active:scale-95'>{border}</span>
                     ))):(
-                        <span className='text-veryDarkBlueText dark:text-whiteText'>No Information</span>
+                        <span className='text-veryDarkBlueText dark:text-veryDarkBlueText'>No Information</span>
                     )
 
                         
