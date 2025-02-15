7772193eaba86967ebcf4216e39b01b7141800f3
salihbezai <salihbezai98@gmail.com>
2025-02-16 00:10:44 +0100

    adding .styleintignore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   ],
   theme: {
   	container: {
-  		//center: 'true',
+  		center: 'true',
   		padding: '2rem',
   		screens: {
   			mobile: '375px',
@@ -83,7 +83,7 @@ const config: Config = {
   			bold: '800'
   		},
   		container: {
-  			//center: 'true',
+  			center: 'true',
   			padding: '2rem',
   			screens: {
   				'2xl': '1400px'
